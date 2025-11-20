fbc1daddb15f9840af4aab9a794b35c1f3b1c85e
Elmir K <edik.halilov1999@gmail.com>
2025-11-20 19:53:33 +0300

    #39 chore: update client (#42)
    Co-authored-by: Elmir Khalilov <elmirkhalilov.dev@gmail.com>

---
+++ client/src/app/router/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '@/pages/home/home';
+
+export const Router = () => {
+	return (
+        <>
+            <Routes>
+                <Route index element={<Home />} />
+            </Routes>
+        </>
+    );
+};
